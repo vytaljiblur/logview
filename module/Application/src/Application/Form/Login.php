42b5ab790d9e6b386d378a59dac881d170ebd473
vitalijiblur <vytalij@blurgroup.com>
2013-07-15 09:56:26 +0100

    All project commit

---
+++ module/Application/src/Application/Form/Login.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Application\Form;
+
+use Zend\Form\Form;
+use Zend\Form\Element;
+use ZfcBase\Form\ProvidesEventsForm;
+use ZfcUser\Options\AuthenticationOptionsInterface;
+use ZfcUser\Module as ZfcUser;
+
+class Login extends ProvidesEventsForm
+{
+    /**
+     * @var AuthenticationOptionsInterface
+     */
+    protected $authOptions;
+
+    public function __construct($name = null)
+    {
+        parent::__construct($name);
+
+        $this->add(array(
+            'name' => 'identity',
+            'options' => array(
+                'label' => '',
+            ),
+            'attributes' => array(
+                'type' => 'text'
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'credential',
+            'options' => array(
+                'label' => 'Password',
+            ),
+            'attributes' => array(
+                'type' => 'password',
+            ),
+        ));
+
+        $this->getEventManager()->trigger('init', $this);
+
+    }
+
+}
